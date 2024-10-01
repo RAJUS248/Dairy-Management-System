a0df8fde43a800d2d49e96bc5ac3206e52bee20f
RAJUS248 <125146841+RAJUS248@users.noreply.github.com>
2024-10-01 22:31:55 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary pt-3">
+  <div class="container-fluid">
+
+    <h1><a class="navbar-brand" href="index.php"></a></h1>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="startpage.php"></a>
+        </li>
+</ul>
+    <form class="d-flex" >
+       
+    </form>
+    
+
+  </div>
+</nav>
